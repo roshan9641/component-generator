7b91bdf32cd13a34d6aafb89c96c220b017386fc
Roshan <roshansingh9641460041@gmail.com>
2025-12-07 20:00:20 +0530

    firstcomit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css"
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from "./pages/Home";
+import NoPage from "./pages/NoPage";
+const App = () => {
+  return (
+    <>
+    <BrowserRouter>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+         <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import {HiSun} from 'react-icons/hi'
+import {FaUser} from 'react-icons/fa'
+import {RiSettings3Fill} from 'react-icons/ri'
+const Navbar = () => {
+  return (
+    <>
+    <div className="nav flex items-center justify-between px-[100px] h-[90px] border-b border-gray-800">
+      <div className="logo">
+        <h3 className="text-[25px] font-[700] sp-text">GenUI</h3>
+      </div>
+      <div className="icons flex items-center gap-[15px]">
+        <div className="icon"> <HiSun /></div>
+        <div className="icon"> <FaUser /></div>
+        <div className="icon"> <RiSettings3Fill /></div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
